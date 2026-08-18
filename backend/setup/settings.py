f887585fc6a329e3ecb9ce336d1638d870d6c342
Matheus Iago <162377902+iagosk@users.noreply.github.com>
2026-08-18 11:14:52 -0300

    Correção das credenciais do banco

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -92,9 +92,9 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dbHabitto',
+        'NAME': 'dbhabitto',
         'PASSWORD':'postgres',
-        'USER':'postgres',
+        'USER':'u0_a315',
         'HOST':'localhost',
         'PORT':'5432',
     }
